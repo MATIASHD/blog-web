c6e979fe55eaf5276000ce676dc27c82959c9c6d
Matias Arevalo <arevalo.matias418@gmail.com>
2026-06-24 08:43:08 -0300

    Se hizo cambio en app.js

--- src/app.js
+++ src/app.js
@@ -4,7 +4,6 @@ const path = require('path');
 const helmet = require('helmet');
 const morgan = require('morgan');
 const session = require('express-session');
-const bcrypt = require('bcrypt');
 
 app.use(helmet());
 app.use(morgan(process.env.NODE_ENV === 'production' ? 'combined' : 'dev'));
